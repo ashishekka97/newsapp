936f718983b2d684c5b13cc3920d7857f8c82aab
Ashish Ekka <ashishekka329@gmail.com>
2020-01-03 08:13:44 +0530

    Properly URLify article titles

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -2,13 +2,15 @@ import React from 'react';
 import styles from './Articles.module.css';
 import Article from '../Article/Article';
 import { Link } from 'react-router-dom';
+import slugify from 'slugify';
 
 const Articles = (props) => {
   return (
     <div className={styles.articles}>
       {
         props.data.map(article => {
-          return <Link to={`details/${article.title}`} key={article.title}><Article data={article}/></Link>
+          const articleTitle = slugify((article.title).toLowerCase(), {remove: /[*+~.()'"!:@]/g});
+          return <Link to={`details/${articleTitle}`} key={articleTitle}><Article data={article}/></Link>
         })
       }
     </div>
