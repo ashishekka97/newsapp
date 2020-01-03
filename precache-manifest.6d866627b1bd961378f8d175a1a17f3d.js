789daf77cf666c6733522775e40b93da5c0b6ad0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-03 03:43:13 +0000

    Deploy AshishEkka97/newsapp to github.com/AshishEkka97/newsapp.git:gh-pages

---
+++ precache-manifest.6d866627b1bd961378f8d175a1a17f3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a7b9335273845ea048cca04a86c6be",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "ef207690fb0bf1297209",
+    "url": "/newsapp/static/css/2.55d40de3.chunk.css"
+  },
+  {
+    "revision": "a6c2f9d6f562a85c88df",
+    "url": "/newsapp/static/css/main.eb780787.chunk.css"
+  },
+  {
+    "revision": "ef207690fb0bf1297209",
+    "url": "/newsapp/static/js/2.9d43c90e.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/newsapp/static/js/2.9d43c90e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6c2f9d6f562a85c88df",
+    "url": "/newsapp/static/js/main.322122ec.chunk.js"
+  },
+  {
+    "revision": "5341653df2e6dbff2583",
+    "url": "/newsapp/static/js/runtime-main.43973354.js"
+  }
+]);
